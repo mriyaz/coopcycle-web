5f20b5c81a5d6fca1e6881b98e45627238570a72
Alexandre Segura <mex.zktk@gmail.com>
2018-06-29 11:55:51 +0200

    Introduce custom authenticator & user provider.

---
+++ src/AppBundle/Security/StoreTokenAuthenticator.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace AppBundle\Security;
+
+use AppBundle\Entity\Store\Token as StoreToken;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class StoreTokenAuthenticator extends AbstractGuardAuthenticator
+{
+    private $tokenRepository;
+
+    public function __construct($doctrine)
+    {
+        $this->tokenRepository = $doctrine->getRepository(StoreToken::class);
+    }
+
+    /**
+     * Called on every request to decide if this authenticator should be
+     * used for the request. Returning false will cause this authenticator
+     * to be skipped.
+     */
+    public function supports(Request $request)
+    {
+        return $request->headers->has('X-Auth-Token');
+    }
+
+    /**
+     * Called on every request. Return whatever credentials you want to
+     * be passed to getUser() as $credentials.
+     */
+    public function getCredentials(Request $request)
+    {
+        return array(
+            'token' => $request->headers->get('X-Auth-Token'),
+        );
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $token = $credentials['token'];
+
+        if (null === $token) {
+            return;
+        }
+
+        $storeToken = $this->tokenRepository->findOneByToken($token);
+
+        if (null === $storeToken) {
+            return;
+        }
+
+        // if a User object, checkCredentials() is called
+        return $userProvider->loadUserByUsername($storeToken);
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        // check credentials - e.g. make sure the password is valid
+        // no credential check is needed in this case
+
+        // return true to cause authentication success
+        return true;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        $store = $this->tokenRepository
+            ->findOneByToken($request->headers->get('X-Auth-Token'))
+            ->getStore();
+
+        $token->setAttribute('store', $store);
+
+        // on success, let the request continue
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = array(
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+
+            // or to translate this message
+            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+        );
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        $data = array(
+            // you might translate this message
+            'message' => 'Authentication Required'
+        );
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function supportsRememberMe()
+    {
+        return false;
+    }
+}
